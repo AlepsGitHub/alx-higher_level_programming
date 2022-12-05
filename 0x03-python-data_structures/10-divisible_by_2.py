1981f42499550d0925d1177d644be89bfa2e70a3
Alebiosu Opeyemi <94289861+AlepsGitHub@users.noreply.github.com>
2022-12-05 14:11:48 +0100

    all
    all

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
+
+
 def divisible_by_2(my_list=[]):
-    if not my_list:
-        return my_list
-    new_list = list(my_list)
-    for index in new_list:
-        if index % 2 == 0:
-            new_list[index] = True
+    list = []
+    for i in my_list:
+        if i % 2:
+            list = list + [False]
         else:
-            new_list[index] = False
-    return new_list
+            list = list + [True]
+    return list
